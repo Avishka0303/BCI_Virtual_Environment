c8325a0698082e0d1c9ed2b9084c61637fa21014
Avishka Dhananjaya <avishka0303@gmail.com>
2021-02-02 19:47:35 +0530

    Add right wall collision trigger.

---
+++ Assets/Scripts/RightWall.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RightWall : MonoBehaviour {
+    private void OnTriggerEnter(Collider other) {
+        print("Sphere collided right wall");
+        Vector3 initPosition = new Vector3(0f,1.91f,-8.68f);
+        other.gameObject.transform.localPosition = initPosition;
+    }
+}
