6e4aae144c94a334cd6a7476f2a82d4dd118e955
Avishka Dhananjaya <avishka0303@gmail.com>
2021-02-02 19:44:36 +0530

    Add left wall collision trigger.

---
+++ Assets/Scripts/LeftWall.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LeftWall: MonoBehaviour {
+    
+    public Text scoreText;
+    private void OnTriggerEnter(Collider other) {
+        print("Sphere collided left wall"+other.gameObject.tag);
+        Vector3 initPosition = new Vector3(0f,1.91f,-15.68f);
+        other.gameObject.transform.position = initPosition;
+        scoreText.text = "Left wall hitted.";
+    }
+}
